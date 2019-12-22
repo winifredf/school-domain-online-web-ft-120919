d713e6f73451560f7a6ed6962b9e3427be03d1db
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 15:09:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  
+  def initializ(school_name)
+    @school_name = school_name
+    @roster = []
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student
+    @student_name = name
+    
+  end
+
+  def grade(number)
+    @grade = grade
+    @roster[grade] = []
+    end
+    
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
