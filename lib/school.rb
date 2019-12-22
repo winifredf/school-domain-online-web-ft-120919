0a68b3704ac8a8ba84bf93f8ca3f18d57a1bea99
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 15:11:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,57 @@
 class School
+  attr_accessor :name, :roster
   
-  def initializ(school_name)
-    @school_name = school_name
-    @roster = []
-  end
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
   
-  def roster
-    @roster
-  end
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
+    roster[level] << student
+  end 
   
-  def add_student
-    @student_name = name
-    
-  end
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
+  
+# This works, but it isn't passing learn.co's tests cause of 
+# how it displays syntactically...but the same information is there
+#  def sort 
+#    roster.sort_by do |x, y| 
+#      x 
+#    end 
+#  end 
 
-  def grade(number)
-    @grade = grade
-    @roster[grade] = []
-    end
-    
-  end
+# Here's learn.co's solution:
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
   
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-  end
-end
+end 
+
+
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
